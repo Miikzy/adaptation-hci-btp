3579cde448bf2d0a73d57a47fc3014fabe2c9c70
Zaki Berouk <zaki.berouk@silex-france.com>
2018-10-30 13:28:19 +0100

    Skeleton with only one view, and a floating button which change the theme

--- app/src/main/java/com/example/zaki_berouk/adaptabtp/MainActivity.java
+++ app/src/main/java/com/example/zaki_berouk/adaptabtp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.zaki_berouk.adaptabtp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,13 +15,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SensorEventListener  {
+
+    private SensorManager mSensorManager;
+    private Sensor mLightSensor;
+    private static float lux;
+    private static float previous_lux = 0;
+    private static int current_theme = R.style.AppTheme;
+    private static boolean isNightMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(current_theme);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -27,8 +41,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                changeTheme(isNightMode);
             }
         });
 
@@ -40,6 +53,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
 
     @Override
@@ -82,7 +100,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_home) {
 
         } else if (id == R.id.nav_slideshow) {
 
@@ -97,5 +115,65 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+//        lux = event.values[0];
+//        TextView current_lux_textview = (TextView) findViewById(R.id.actual_lux);
+//        current_lux_textview.setText(Float.toString(lux));
+//        TextView current_theme_textview = (TextView) findViewById(R.id.actual_theme);
+//        if (lux + 20 >= previous_lux || lux - 20 <= previous_lux) {
+//            previous_lux = lux;
+//            if (lux <= 60) {
+//                current_theme_textview.setText("Dark Theme");
+//                if (current_theme != R.style.AppTheme_Dark) {
+//                    current_theme = R.style.AppTheme_Dark;
+//                    MainActivity.this.recreate();
+//                }
+//            } else {
+//                current_theme_textview.setText("Light Theme");
+//                if (current_theme != R.style.AppTheme) {
+//                    current_theme = R.style.AppTheme;
+//                    MainActivity.this.recreate();
+//                }
+//            }
+//        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void changeTheme(boolean isNightMode){
+        if(isNightMode){
+            this.current_theme = R.style.AppTheme;
+            this.isNightMode = !isNightMode;
+
+        } else {
+            this.current_theme = R.style.AppTheme_Dark;
+            this.isNightMode = !isNightMode;
+        }
+
+        MainActivity.this.recreate();
     }
 }
